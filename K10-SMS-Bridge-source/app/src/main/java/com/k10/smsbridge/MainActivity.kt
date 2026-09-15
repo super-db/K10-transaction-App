2e19434e2a2bee87a988cc95dace0cc9adad3c12
super-db <56974953+super-db@users.noreply.github.com>
2026-09-15 23:33:46 +0530

    Connect Firebase, app login and payer exclusion rules

--- K10-SMS-Bridge-source/app/src/main/java/com/k10/smsbridge/MainActivity.kt
+++ K10-SMS-Bridge-source/app/src/main/java/com/k10/smsbridge/MainActivity.kt
@@ -5,13 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
-import com.k10.smsbridge.ui.BridgeApp
+import com.k10.smsbridge.ui.K10PayApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme(colorScheme = lightColorScheme()) { BridgeApp() }
+            MaterialTheme(colorScheme = lightColorScheme()) { K10PayApp() }
         }
     }
 }
